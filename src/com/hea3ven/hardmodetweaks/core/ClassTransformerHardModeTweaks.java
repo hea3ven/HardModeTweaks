16e7f815ada532b0e650106bce76c400615eceed
Hea3veN <hea3venmc@gmail.com>
2014-04-22 21:17:29 -0300

    Fixed a synchronization issue with the world time.

--- src/com/hea3ven/hardmodetweaks/core/ClassTransformerHardModeTweaks.java
+++ src/com/hea3ven/hardmodetweaks/core/ClassTransformerHardModeTweaks.java
@@ -34,8 +34,11 @@ public class ClassTransformerHardModeTweaks implements IClassTransformer {
 	private static final String WORLD_INFO_OBF_CLASS = "a";
 	private static final String WORLD_INFO_GET_WORLD_TIME_METHOD = "getWorldTime";
 	private static final String WORLD_INFO_GET_WORLD_TIME_OBF_METHOD = "a";
+	private static final String WORLD_INFO_SET_WORLD_TIME_METHOD = "setWorldTime";
+	private static final String WORLD_INFO_SET_WORLD_TIME_OBF_METHOD = "a";
 
-	private Logger logger = LogManager.getLogger("HardModeTweaks.ClassTransformer");
+	private Logger logger = LogManager
+			.getLogger("HardModeTweaks.ClassTransformer");
 
 	@Override
 	public byte[] transform(String name, String transformedName,
@@ -92,10 +95,13 @@ public class ClassTransformerHardModeTweaks implements IClassTransformer {
 		while (iter.hasNext()) {
 			AbstractInsnNode currentNode = iter.next();
 
-			if (currentNode.getOpcode() == Opcodes.INVOKEVIRTUAL
-					&& ((MethodInsnNode) currentNode).name
-							.equals("getWorldTime")) {
-				((MethodInsnNode) currentNode).name = "getRealWorldTime";
+			if (currentNode.getOpcode() == Opcodes.INVOKEVIRTUAL) {
+				MethodInsnNode methodInsnNode = (MethodInsnNode) currentNode;
+				if (methodInsnNode.name.equals("getWorldTime")) {
+					methodInsnNode.name = "getRealWorldTime";
+				} else if (methodInsnNode.name.equals("setWorldTime")) {
+					methodInsnNode.name = "setRealWorldTime";
+				}
 			}
 		}
 	}
@@ -123,29 +129,55 @@ public class ClassTransformerHardModeTweaks implements IClassTransformer {
 		Iterator<AbstractInsnNode> iter = method.instructions.iterator();
 
 		// Replace
-		// > this.getWorldTime()
+		// > this.setWorldTime(this.getWorldTime() + 1L);
 		// To
-		// > this.provider.worldObj.getWorldInfo().getRealWorldTime()
+		// > this.provider.worldObj.getWorldInfo().setWorldTime(
+		// >         this.provider.worldObj.getWorldInfo().getRealWorldTime() + 1L);
 		while (iter.hasNext()) {
 			AbstractInsnNode currentNode = iter.next();
 
-			if (currentNode.getOpcode() == Opcodes.INVOKEVIRTUAL
-					&& ((MethodInsnNode) currentNode).name
-							.equals("getWorldTime")) {
-				((MethodInsnNode) currentNode).name = "getRealWorldTime";
-				((MethodInsnNode) currentNode).owner = "net/minecraft/world/storage/WorldInfo";
-				method.instructions.insertBefore(currentNode,
-						new FieldInsnNode(Opcodes.GETFIELD,
-								"net/minecraft/world/World", "provider",
-								"Lnet/minecraft/world/WorldProvider;"));
-				method.instructions.insertBefore(currentNode,
-						new FieldInsnNode(Opcodes.GETFIELD,
-								"net/minecraft/world/WorldProvider",
-								"worldObj", "Lnet/minecraft/world/World;"));
-				method.instructions.insertBefore(currentNode,
-						new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
-								"net/minecraft/world/World", "getWorldInfo",
-								"()Lnet/minecraft/world/storage/WorldInfo;"));
+			if (currentNode.getOpcode() == Opcodes.INVOKEVIRTUAL) {
+				MethodInsnNode methodInsnNode = (MethodInsnNode) currentNode;
+				if (methodInsnNode.name.equals("getWorldTime")) {
+					methodInsnNode.name = "getRealWorldTime";
+					methodInsnNode.owner = "net/minecraft/world/storage/WorldInfo";
+
+					method.instructions.insertBefore(currentNode.getPrevious(),
+							new FieldInsnNode(Opcodes.GETFIELD,
+									"net/minecraft/world/World", "provider",
+									"Lnet/minecraft/world/WorldProvider;"));
+					method.instructions.insertBefore(currentNode.getPrevious(),
+							new FieldInsnNode(Opcodes.GETFIELD,
+									"net/minecraft/world/WorldProvider",
+									"worldObj", "Lnet/minecraft/world/World;"));
+					method.instructions
+							.insertBefore(
+									currentNode.getPrevious(),
+									new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
+											"net/minecraft/world/World",
+											"getWorldInfo",
+											"()Lnet/minecraft/world/storage/WorldInfo;"));
+
+					method.instructions.insertBefore(currentNode,
+							new FieldInsnNode(Opcodes.GETFIELD,
+									"net/minecraft/world/World", "provider",
+									"Lnet/minecraft/world/WorldProvider;"));
+					method.instructions.insertBefore(currentNode,
+							new FieldInsnNode(Opcodes.GETFIELD,
+									"net/minecraft/world/WorldProvider",
+									"worldObj", "Lnet/minecraft/world/World;"));
+					method.instructions
+							.insertBefore(
+									currentNode,
+									new MethodInsnNode(Opcodes.INVOKEVIRTUAL,
+											"net/minecraft/world/World",
+											"getWorldInfo",
+											"()Lnet/minecraft/world/storage/WorldInfo;"));
+				} else if (methodInsnNode.name.equals("setWorldTime")) {
+					methodInsnNode.name = "setRealWorldTime";
+					methodInsnNode.owner = "net/minecraft/world/storage/WorldInfo";
+
+				}
 			}
 		}
 	}
@@ -166,16 +198,29 @@ public class ClassTransformerHardModeTweaks implements IClassTransformer {
 		getWorldTimeMethod.name = "getRealWorldTime";
 		logger.info("Finished renaming getWorldTime method of WorldInfo");
 
+		logger.info("Renaming setWorldTime method of WorldInfo to setRealWorldTime");
+		MethodNode setWorldTimeMethod = getMethod(classNode,
+				obfuscated ? WORLD_INFO_SET_WORLD_TIME_OBF_METHOD
+						: WORLD_INFO_SET_WORLD_TIME_METHOD, "(J)V");
+		if (setWorldTimeMethod == null)
+			error("Could not find the method");
+		setWorldTimeMethod.name = "setRealWorldTime";
+		logger.info("Finished renaming setWorldTime method of WorldInfo");
+
 		logger.info("Creating new implementation of getWorldTime method of WorldInfo");
 		classNode.methods.add(createNewGetWorldTimeMethod());
 		logger.info("Finished adding getWorldTime method to WorldInfo");
+
+		logger.info("Creating new implementation of setWorldTime method of WorldInfo");
+		classNode.methods.add(createNewSetWorldTimeMethod());
+		logger.info("Finished adding setWorldTime method to WorldInfo");
 
 		return writeClass(classNode);
 	}
 
 	private MethodNode createNewGetWorldTimeMethod() {
-		// > double getWorldTime() {
-		// >     return Math.floor(worldTime * 2.0d);
+		// > long getWorldTime() {
+		// >     return Math.floor(worldTime * 0.5d);
 		// > }
 		MethodNode getWorldTimeMethod = new MethodNode(Opcodes.ASM4,
 				Opcodes.ACC_PUBLIC, "getWorldTime", "()J", null, null);
@@ -183,13 +228,33 @@ public class ClassTransformerHardModeTweaks implements IClassTransformer {
 		getWorldTimeMethod.instructions.add(new FieldInsnNode(Opcodes.GETFIELD,
 				"net/minecraft/world/storage/WorldInfo", "worldTime", "J"));
 		getWorldTimeMethod.instructions.add(new InsnNode(Opcodes.L2D));
-		getWorldTimeMethod.instructions.add(new LdcInsnNode(2.0d));
+		getWorldTimeMethod.instructions.add(new LdcInsnNode(0.5d));
 		getWorldTimeMethod.instructions.add(new InsnNode(Opcodes.DMUL));
 		getWorldTimeMethod.instructions.add(new MethodInsnNode(
 				Opcodes.INVOKESTATIC, "java/lang/Math", "floor", "(D)D"));
 		getWorldTimeMethod.instructions.add(new InsnNode(Opcodes.D2L));
 		getWorldTimeMethod.instructions.add(new InsnNode(Opcodes.LRETURN));
 		return getWorldTimeMethod;
+	}
+
+	private MethodNode createNewSetWorldTimeMethod() {
+		// > void setWorldTime(long time) {
+		// >     worldTime = (long)Math.floor(time / 2.0d);
+		// > }
+		MethodNode setWorldTimeMethod = new MethodNode(Opcodes.ASM4,
+				Opcodes.ACC_PUBLIC, "setWorldTime", "(J)V", null, null);
+		setWorldTimeMethod.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
+		setWorldTimeMethod.instructions.add(new VarInsnNode(Opcodes.LLOAD, 1));
+		setWorldTimeMethod.instructions.add(new InsnNode(Opcodes.L2D));
+		setWorldTimeMethod.instructions.add(new LdcInsnNode(0.5d));
+		setWorldTimeMethod.instructions.add(new InsnNode(Opcodes.DDIV));
+		setWorldTimeMethod.instructions.add(new MethodInsnNode(
+				Opcodes.INVOKESTATIC, "java/lang/Math", "floor", "(D)D"));
+		setWorldTimeMethod.instructions.add(new InsnNode(Opcodes.D2L));
+		setWorldTimeMethod.instructions.add(new FieldInsnNode(Opcodes.PUTFIELD,
+				"net/minecraft/world/storage/WorldInfo", "worldTime", "J"));
+		setWorldTimeMethod.instructions.add(new InsnNode(Opcodes.RETURN));
+		return setWorldTimeMethod;
 	}
 
 	//
