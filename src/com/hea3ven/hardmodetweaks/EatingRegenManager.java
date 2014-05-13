25596b91f40152684f6f98a36d46520f0463bd4c
Hea3veN <hea3venmc@gmail.com>
2014-05-12 21:02:43 -0300

    Prevent healing when eating a food with a negative potion effect.

--- src/com/hea3ven/hardmodetweaks/EatingRegenManager.java
+++ src/com/hea3ven/hardmodetweaks/EatingRegenManager.java
@@ -21,7 +21,10 @@
 
 package com.hea3ven.hardmodetweaks;
 
+import java.lang.reflect.Field;
+
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
 
@@ -38,12 +41,18 @@ public class EatingRegenManager {
 	public int foodHealingMinimum = 3;
 	public double foodHealingMultiplier = 0.3d;
 
+	private static final int HUNGER_POTION_ID = 17;
+	private static final int POISON_POTION_ID = 19;
+	private static final int WITHER_POTION_ID = 20;
+
 	@SubscribeEvent
 	public void playerUseItemFinished(PlayerUseItemEvent.Finish e) {
 		logger.debug("Event PlayerUseItemEvent.Finish received");
 		if (e.item.getItem().getItemUseAction(e.item) == EnumAction.eat
 				&& e.item.getItem() instanceof ItemFood) {
 			logger.debug("Finished eating");
+			if (negativePotionEffect(e.item.getItem()))
+				return;
 			float healAmount = (float) Math.ceil((((ItemFood) e.item.getItem())
 					.func_150905_g(e.item) - foodHealingMinimum)
 					* foodHealingMultiplier);
@@ -52,6 +61,42 @@ public class EatingRegenManager {
 				e.entityPlayer.heal(healAmount);
 			}
 		}
+	}
+
+	private boolean negativePotionEffect(Item item) {
+		int foodPotionId = getFoodPotionId(item);
+		return foodPotionId == HUNGER_POTION_ID
+				|| foodPotionId == POISON_POTION_ID
+				|| foodPotionId == WITHER_POTION_ID;
+	}
+
+	private int getFoodPotionId(Item item) {
+		Field potionIdField = null;
+		try {
+			potionIdField = ItemFood.class.getDeclaredField("field_77851_ca");
+		} catch (NoSuchFieldException e1) {
+			logger.error("could not get 'field_77851_ca' field", e1);
+			try {
+				potionIdField = ItemFood.class.getDeclaredField("potionId");
+			} catch (NoSuchFieldException e2) {
+				logger.error("could not get 'potionId' field", e1);
+			} catch (SecurityException e2) {
+				logger.error("could not get 'potionId' field", e1);
+			}
+		} catch (SecurityException e1) {
+			logger.error("could not get 'field_77851_ca' field", e1);
+		}
+		if (potionIdField != null) {
+			try {
+				potionIdField.setAccessible(true);
+				return (Integer) potionIdField.get(item);
+			} catch (IllegalArgumentException e1) {
+				logger.error("could not set 'potionId' field's value", e1);
+			} catch (IllegalAccessException e1) {
+				logger.error("could not set 'potionId' field's value", e1);
+			}
+		}
+		return 0;
 	}
 
 }
