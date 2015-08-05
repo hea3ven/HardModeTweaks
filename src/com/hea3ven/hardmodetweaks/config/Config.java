62fdeef85cd372c2ad980cd275ecc4a3e6b27e11
Hea3veN <hea3venmc@gmail.com>
2015-08-05 07:28:28 -0300

    sync config with the ASMTweaks config

--- src/com/hea3ven/hardmodetweaks/config/Config.java
+++ src/com/hea3ven/hardmodetweaks/config/Config.java
@@ -17,181 +17,207 @@ import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 
+import com.hea3ven.tweaks.DayNightCycle;
+import com.hea3ven.tweaks.Hea3venTweaks;
+
 public class Config {
-    public static boolean enableDayCycleTweaks = true;
-    public static double dayLengthMultiplier;
-    public static float dayToNightRatio;
+	public static boolean enableTweakAnimalAI;
+	public static boolean enableEatingHeal;
 
-    public static boolean enableTweakAnimalAI;
-    public static boolean enableEatingHeal;
+	public static int requiredFoodValue;
+	public static float healValueOffset;
+	public static float healValueMultiplier;
 
-    public static int requiredFoodValue;
-    public static float healValueOffset;
-    public static float healValueMultiplier;
+	public static boolean enableMobsTweaks;
+	public static boolean spidersApplySlowness;
+	public static boolean replaceCaveSpiderPoison;
 
-    public static boolean enableMobsTweaks;
-    public static boolean spidersApplySlowness;
-    public static boolean replaceCaveSpiderPoison;
+	public static boolean enableGameRules;
+	public static Map<String, String> gameRules;
 
-    public static boolean enableGameRules;
-    public static Map<String, String> gameRules;
+	private static Config INSTANCE;
 
-    private static Config INSTANCE;
+	private File configfile;
+	private Configuration generalConfig;
 
-    private File configfile;
-    private Configuration generalConfig;
+	private Property enableDayCycleTweaksProp;
+	private Property cycleLengthMultiplierProp;
+	private Property dayToNightRatioProp;
 
-    private Property enableDayCycleTweaksProp;
-    private Property cycleLengthMultiplierProp;
-    private Property dayToNightRatioProp;
+	private Property enableEatingHealProp;
+	private Property enableTweakAnimalAIProp;
+	private Property enablePreventBoatBreakProp;
+	private Property enableNonSolidLeavesProp;
 
-    private Property enableEatingHealProp;
-    private Property enableTweakAnimalAIProp;
+	private Property requiredFoodValueProp;
+	private Property healValueOffsetProp;
+	private Property healValueMultiplierProp;
 
-    private Property requiredFoodValueProp;
-    private Property healValueOffsetProp;
-    private Property healValueMultiplierProp;
+	private Property enableMobsTweaksProp;
+	private Property spidersApplySlownessProp;
+	private Property replaceCaveSpiderPoisonProp;
 
-    private Property enableMobsTweaksProp;
-    private Property spidersApplySlownessProp;
-    private Property replaceCaveSpiderPoisonProp;
+	private Property enableGameRulesProp;
+	private Map<String, Property> gameRulesProps;
 
-    private Property enableGameRulesProp;
-    private Map<String, Property> gameRulesProps;
+	private Config(File configFile) {
+		this.configfile = configFile;
+		generalConfig = new Configuration(configfile, true);
+	}
 
-    private Config(File configFile) {
-        this.configfile = configFile;
-        generalConfig = new Configuration(configfile, true);
-    }
+	public static void init(File configFile) {
+		try {
+			INSTANCE = new Config(configFile);
+			INSTANCE.read();
+			INSTANCE.initProperties();
+			INSTANCE.load();
+			INSTANCE.save();
+		} catch (Exception e) {
+			FMLLog.log(Level.FATAL, e, "HardModeTweaks' configuration failed to load.");
+		}
+	}
 
-    public static void init(File configFile) {
-        try {
-            INSTANCE = new Config(configFile);
-            INSTANCE.read();
-            INSTANCE.initProperties();
-            INSTANCE.load();
-            INSTANCE.save();
-        } catch (Exception e) {
-            FMLLog.log(Level.FATAL, e, "HardModeTweaks' configuration failed to load.");
-        }
-    }
+	public static void reload() {
+		INSTANCE.load();
+		INSTANCE.save();
+	}
 
-    public static void reload() {
-        INSTANCE.load();
-        INSTANCE.save();
-    }
+	private void initProperties() {
+		generalConfig.getCategory("DayNightCycle").setLanguageKey(
+				"hardmodetweaks.config.daynightcycle.cat");
+		generalConfig
+				.getCategory("FoodHealing")
+				.setLanguageKey("hardmodetweaks.config.foodheal.cat");
+		generalConfig.getCategory("Mobs").setLanguageKey("hardmodetweaks.config.mobs.cat");
+		generalConfig
+				.getCategory("GameRules")
+				.setLanguageKey("hardmodetweaks.config.gamerules.cat");
+		generalConfig.getCategory("Other").setLanguageKey("hardmodetweaks.config.other.cat");
 
-    private void initProperties() {
-        generalConfig.getCategory("DayNightCycle").setLanguageKey(
-                "hardmodetweaks.config.daynightcycle.cat");
-        generalConfig.getCategory("FoodHealing").setLanguageKey(
-                "hardmodetweaks.config.foodheal.cat");
-        generalConfig.getCategory("Mobs").setLanguageKey("hardmodetweaks.config.mobs.cat");
-        generalConfig.getCategory("GameRules")
-                .setLanguageKey("hardmodetweaks.config.gamerules.cat");
-        generalConfig.getCategory("Other").setLanguageKey("hardmodetweaks.config.other.cat");
+		enableDayCycleTweaksProp = generalConfig
+				.get("DayNightCycle", "enableDayCycleTweaks", true,
+						"Enable the day/night cycle tweaks")
+				.setLanguageKey("hardmodetweaks.config.daynightcycle.enableDayCycleTweaks")
+				.setRequiresWorldRestart(true);
+		cycleLengthMultiplierProp = generalConfig
+				.get("DayNightCycle", "cycleLengthMultiplier", 1.0d,
+						"Change the length of the day/night cycle, 1.0 is the same as vanilla, which is 20 minutes.")
+				.setLanguageKey("hardmodetweaks.config.daynightcycle.cycleLenghtMult")
+				.setRequiresWorldRestart(true);
+		dayToNightRatioProp = generalConfig
+				.get("DayNightCycle", "dayToNightRatio", 0.5d,
+						"Ratio between the length of the day and the length of the night, values should be between 0.0 and 1.0. A value of 0.5 means day and night are the same length, a value of 0.75 means the day is longer than the night.")
+				.setLanguageKey("hardmodetweaks.config.daynightcycle.daynightratio")
+				.setRequiresWorldRestart(true);
 
-        enableDayCycleTweaksProp = generalConfig
-                .get("DayNightCycle", "enableDayCycleTweaks", true,
-                        "Enable the day/night cycle tweaks")
-                .setLanguageKey("hardmodetweaks.config.daynightcycle.enableDayCycleTweaks")
-                .setRequiresWorldRestart(true);
-        cycleLengthMultiplierProp = generalConfig
-                .get("DayNightCycle", "cycleLengthMultiplier", 1.0d,
-                        "Change the length of the day/night cycle, 1.0 is the same as vanilla, which is 20 minutes.")
-                .setLanguageKey("hardmodetweaks.config.daynightcycle.cycleLenghtMult")
-                .setRequiresWorldRestart(true);
-        dayToNightRatioProp = generalConfig
-                .get("DayNightCycle",
-                        "dayToNightRatio",
-                        0.5d,
-                        "Ratio between the length of the day and the length of the night, values should be between 0.0 and 1.0. A value of 0.5 means day and night are the same length, a value of 0.75 means the day is longer than the night.")
-                .setLanguageKey("hardmodetweaks.config.daynightcycle.daynightratio")
-                .setRequiresWorldRestart(true);
+		enableEatingHealProp = generalConfig
+				.get("FoodHealing", "enableFoodHealing", true,
+						"Enable the healing when you eat food.")
+				.setLanguageKey("hardmodetweaks.config.foodheal.enable");
+		requiredFoodValueProp = generalConfig
+				.get("FoodHealing", "requiredFoodValue", 3,
+						"The required value of the food to apply the heal.")
+				.setLanguageKey("hardmodetweaks.config.foodheal.requiredvalue");
+		healValueOffsetProp = generalConfig.get("FoodHealing", "healValueOffset", 1).setLanguageKey(
+				"hardmodetweaks.config.foodheal.healoffset");
+		healValueMultiplierProp = generalConfig
+				.get("FoodHealing", "healValueMultiplier", 0.3d,
+						"The formula for the heal is (FoodValue - healValueOffset) * healValueMultiplier")
+				.setLanguageKey("hardmodetweaks.config.foodheal.healmultiplier");
 
-        enableEatingHealProp = generalConfig.get("FoodHealing", "enableFoodHealing", true,
-                "Enable the healing when you eat food.").setLanguageKey(
-                "hardmodetweaks.config.foodheal.enable");
-        requiredFoodValueProp = generalConfig.get("FoodHealing", "requiredFoodValue", 3,
-                "The required value of the food to apply the heal.").setLanguageKey(
-                "hardmodetweaks.config.foodheal.requiredvalue");
-        healValueOffsetProp = generalConfig.get("FoodHealing", "healValueOffset", 1)
-                .setLanguageKey("hardmodetweaks.config.foodheal.healoffset");
-        healValueMultiplierProp = generalConfig.get("FoodHealing", "healValueMultiplier", 0.3d,
-                "The formula for the heal is (FoodValue - healValueOffset) * healValueMultiplier")
-                .setLanguageKey("hardmodetweaks.config.foodheal.healmultiplier");
+		enableTweakAnimalAIProp = generalConfig
+				.get("Other", "enableAnimalAITweak", true,
+						"Enable changing the animals AI to make them run from their attackers.")
+				.setLanguageKey("hardmodetweaks.config.other.animalaitweak");
+		enablePreventBoatBreakProp = generalConfig
+				.get("Other", "enablePreventBoatBreak", true,
+						"Enable to make it so that boats don't break when hitting blocks.")
+				.setLanguageKey("hardmodetweaks.config.other.preventboatbreak")
+				.setRequiresWorldRestart(true);
+		enableNonSolidLeavesProp = generalConfig
+				.get("Other", "enableNonSolidLeaves", true,
+						"Enable tweak to make leaves blocks non-solid (like vines).")
+				.setLanguageKey("hardmodetweaks.config.other.nonsolidleaves")
+				.setRequiresWorldRestart(true);
 
-        enableTweakAnimalAIProp = generalConfig.get("Other", "enableAnimalAITweak", true,
-                "Enable changing the animals AI to make them run from their attackers.")
-                .setLanguageKey("hardmodetweaks.config.other.animalaitweak");
+		enableMobsTweaksProp = generalConfig
+				.get("Mobs", "enableMobsTweak", true, "Enable tweaks for mobs.")
+				.setLanguageKey("hardmodetweaks.config.mobs.enableMobsTweak");
+		spidersApplySlownessProp = generalConfig
+				.get("Mobs", "spidersApplySlowness", true,
+						"Enable to make spiders apply slowness when they attack.")
+				.setLanguageKey("hardmodetweaks.config.mobs.spidersApplySlowness");
+		replaceCaveSpiderPoisonProp = generalConfig
+				.get("Mobs", "replaceCaveSpiderPoison", true,
+						"Enable to make it so cave spiders apply weakness in stead of poison.")
+				.setLanguageKey("hardmodetweaks.config.mobs.replaceCaveSpiderPoison");
 
-        enableMobsTweaksProp = generalConfig.get("Mobs", "enableMobsTweak", true,
-                "Enable tweaks for mobs.").setLanguageKey(
-                "hardmodetweaks.config.mobs.enableMobsTweak");
-        spidersApplySlownessProp = generalConfig.get("Mobs", "spidersApplySlowness", true,
-                "Enable to make spiders apply slowness when they attack.").setLanguageKey(
-                "hardmodetweaks.config.mobs.spidersApplySlowness");
-        replaceCaveSpiderPoisonProp = generalConfig.get("Mobs", "replaceCaveSpiderPoison", true,
-                "Enable to make it so cave spiders apply weakness in stead of poison.")
-                .setLanguageKey("hardmodetweaks.config.mobs.replaceCaveSpiderPoison");
+		enableGameRulesProp = generalConfig
+				.get("GameRules", "enableGameRules", true, "Enable changing the game rules.")
+				.setLanguageKey("hardmodetweaks.config.gamerules.enable")
+				.setRequiresWorldRestart(true);
+		gameRulesProps = new HashMap<String, Property>();
+		GameRules rules = new GameRules();
+		for (String ruleName : rules.getRules()) {
+			gameRulesProps.put(ruleName,
+					generalConfig
+							.get("GameRules", ruleName, rules.getGameRuleStringValue(ruleName))
+							.setRequiresWorldRestart(true));
+		}
+	}
 
-        enableGameRulesProp = generalConfig
-                .get("GameRules", "enableGameRules", true, "Enable changing the game rules.")
-                .setLanguageKey("hardmodetweaks.config.gamerules.enable")
-                .setRequiresWorldRestart(true);
-        gameRulesProps = new HashMap<String, Property>();
-        GameRules rules = new GameRules();
-        for (String ruleName : rules.getRules()) {
-            gameRulesProps.put(ruleName,
-                    generalConfig
-                            .get("GameRules", ruleName, rules.getGameRuleStringValue(ruleName))
-                            .setRequiresWorldRestart(true));
-        }
-    }
+	private void read() {
+		generalConfig.load();
+	}
 
-    private void read() {
-        generalConfig.load();
-    }
+	private void load() {
+		Hea3venTweaks.asmTweaksManager.getConfig().setConfig("DayNightCycle.enabled",
+				Boolean.toString(enableDayCycleTweaksProp.getBoolean()));
+		Hea3venTweaks.asmTweaksManager.getConfig().setConfig("DayNightCycle.cycleLengthMultiplier",
+				Double.toString(cycleLengthMultiplierProp.getDouble()));
+		DayNightCycle.dayLengthMultiplier = 1.0d / cycleLengthMultiplierProp.getDouble();
+		Hea3venTweaks.asmTweaksManager.getConfig().setConfig("DayNightCycle.dayToNightRatio",
+				Float.toString((float) dayToNightRatioProp.getDouble()));
+		DayNightCycle.dayToNightRatio = 2.0f * (float) dayToNightRatioProp.getDouble();
 
-    private void load() {
-        Config.enableDayCycleTweaks = enableDayCycleTweaksProp.getBoolean();
-        Config.dayLengthMultiplier = 1.0d / cycleLengthMultiplierProp.getDouble();
-        Config.dayToNightRatio = 2.0f * (float) dayToNightRatioProp.getDouble();
+		Config.enableTweakAnimalAI = enableTweakAnimalAIProp.getBoolean();
+		Hea3venTweaks.asmTweaksManager.getConfig().setConfig("PreventBoatBreak.enabled",
+				Boolean.toString(enablePreventBoatBreakProp.getBoolean()));
+		Hea3venTweaks.asmTweaksManager.getConfig().setConfig("NonSolidLeaves.enabled",
+				Boolean.toString(enableNonSolidLeavesProp.getBoolean()));
 
-        Config.enableTweakAnimalAI = enableTweakAnimalAIProp.getBoolean();
+		Config.enableEatingHeal = enableEatingHealProp.getBoolean();
+		Config.requiredFoodValue = requiredFoodValueProp.getInt();
+		Config.healValueOffset = (float) healValueOffsetProp.getDouble();
+		Config.healValueMultiplier = (float) healValueMultiplierProp.getDouble();
 
-        Config.enableEatingHeal = enableEatingHealProp.getBoolean();
-        Config.requiredFoodValue = requiredFoodValueProp.getInt();
-        Config.healValueOffset = (float) healValueOffsetProp.getDouble();
-        Config.healValueMultiplier = (float) healValueMultiplierProp.getDouble();
+		Config.enableMobsTweaks = enableMobsTweaksProp.getBoolean();
+		Config.spidersApplySlowness = spidersApplySlownessProp.getBoolean();
+		Config.replaceCaveSpiderPoison = replaceCaveSpiderPoisonProp.getBoolean();
 
-        Config.enableMobsTweaks = enableMobsTweaksProp.getBoolean();
-        Config.spidersApplySlowness = spidersApplySlownessProp.getBoolean();
-        Config.replaceCaveSpiderPoison = replaceCaveSpiderPoisonProp.getBoolean();
+		Config.enableGameRules = enableGameRulesProp.getBoolean(true);
+		Config.gameRules = new HashMap<String, String>();
+		for (String ruleName : gameRulesProps.keySet()) {
+			Config.gameRules.put(ruleName, gameRulesProps.get(ruleName).getString());
+		}
+	}
 
-        Config.enableGameRules = enableGameRulesProp.getBoolean(true);
-        Config.gameRules = new HashMap<String, String>();
-        for (String ruleName : gameRulesProps.keySet()) {
-            Config.gameRules.put(ruleName, gameRulesProps.get(ruleName).getString());
-        }
-    }
+	private void save() {
+		if (generalConfig.hasChanged())
+			generalConfig.save();
+	}
 
-    private void save() {
-        if (generalConfig.hasChanged())
-            generalConfig.save();
-    }
+	public static List<IConfigElement> getElements() {
+		return INSTANCE.getConfigElements();
+	}
 
-    public static List<IConfigElement> getElements() {
-        return INSTANCE.getConfigElements();
-    }
-
-    private List<IConfigElement> getConfigElements() {
-        List<IConfigElement> elems = new ArrayList<IConfigElement>();
-        elems.add(new ConfigElement(generalConfig.getCategory("DayNightCycle")));
-        elems.add(new ConfigElement(generalConfig.getCategory("FoodHealing")));
-        elems.add(new ConfigElement(generalConfig.getCategory("GameRules")));
-        elems.add(new ConfigElement(generalConfig.getCategory("Mobs")));
-        elems.add(new ConfigElement(generalConfig.getCategory("Other")));
-        return elems;
-    }
+	private List<IConfigElement> getConfigElements() {
+		List<IConfigElement> elems = new ArrayList<IConfigElement>();
+		elems.add(new ConfigElement(generalConfig.getCategory("DayNightCycle")));
+		elems.add(new ConfigElement(generalConfig.getCategory("FoodHealing")));
+		elems.add(new ConfigElement(generalConfig.getCategory("GameRules")));
+		elems.add(new ConfigElement(generalConfig.getCategory("Mobs")));
+		elems.add(new ConfigElement(generalConfig.getCategory("Other")));
+		return elems;
+	}
 }
