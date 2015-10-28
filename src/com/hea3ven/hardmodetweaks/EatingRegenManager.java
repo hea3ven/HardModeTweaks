a350fed25c2a9c5a3d20ed83d522c52e805fcdd8
Hea3veN <hea3venmc@gmail.com>
2015-10-27 21:08:48 -0300

    Added config to modify foods values and saturation

--- src/com/hea3ven/hardmodetweaks/EatingRegenManager.java
+++ src/com/hea3ven/hardmodetweaks/EatingRegenManager.java
@@ -26,9 +26,11 @@ import org.apache.logging.log4j.Logger;
 
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFishFood.FishType;
 import net.minecraft.item.ItemFood;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
@@ -37,7 +39,7 @@ import com.hea3ven.hardmodetweaks.config.Config;
 
 public class EatingRegenManager {
 
-    private Logger logger = LogManager.getLogger("HardModeTweaks.EatingRegenManager");
+    private static final Logger logger = LogManager.getLogger("HardModeTweaks.EatingRegenManager");
 
     private static final int HUNGER_POTION_ID = 17;
     private static final int POISON_POTION_ID = 19;
@@ -49,6 +51,7 @@ public class EatingRegenManager {
         if (Config.enableEatingHeal) {
             if (instance == null) {
                 instance = new EatingRegenManager();
+                instance.modifyFoodValues();
                 MinecraftForge.EVENT_BUS.register(instance);
             }
         } else {
@@ -57,6 +60,37 @@ public class EatingRegenManager {
                 instance = null;
             }
         }
+    }
+
+    public void modifyFoodValues() {
+        for (FoodConfig foodCfg : Config.foodValuesConfig) {
+            if (foodCfg.getName().equals("minecraft:raw_fish")) {
+                modifyFishFoodValue(FishType.COD, foodCfg, false);
+            } else if (foodCfg.getName().equals("minecraft:cooked_fish")) {
+                modifyFishFoodValue(FishType.COD, foodCfg, true);
+            } else if (foodCfg.getName().equals("minecraft:raw_salmon")) {
+                modifyFishFoodValue(FishType.SALMON, foodCfg, false);
+            } else if (foodCfg.getName().equals("minecraft:cooked_salmon")) {
+                modifyFishFoodValue(FishType.SALMON, foodCfg, true);
+            } else {
+                Item item = (Item) Item.itemRegistry.getObject(foodCfg.getName());
+                if (!(item instanceof ItemFood)) {
+                    logger.warn("The item '{}' is not a food item", foodCfg.getName());
+                    continue;
+                }
+                ItemFood food = (ItemFood) item;
+
+                ReflectionHelper.setPrivateValue(ItemFood.class, food, foodCfg.getValue(), 1);
+                ReflectionHelper.setPrivateValue(ItemFood.class, food, foodCfg.getSaturation(), 2);
+            }
+        }
+    }
+
+    private void modifyFishFoodValue(FishType fishType, FoodConfig foodCfg, boolean cooked) {
+        ReflectionHelper.setPrivateValue(FishType.class, fishType, foodCfg.getValue(),
+                !cooked ? 9 : 11);
+        ReflectionHelper.setPrivateValue(FishType.class, fishType, foodCfg.getSaturation(),
+                !cooked ? 10 : 12);
     }
 
     @SubscribeEvent
@@ -82,4 +116,44 @@ public class EatingRegenManager {
         return foodPotionId == HUNGER_POTION_ID || foodPotionId == POISON_POTION_ID
                 || foodPotionId == WITHER_POTION_ID;
     }
+
+    public static class FoodConfig {
+        private String name;
+        private int value;
+        private float saturation;
+
+        public FoodConfig(String name, int value, float saturation) {
+            this.name = name;
+            this.value = value;
+            this.saturation = saturation;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public float getSaturation() {
+            return saturation;
+        }
+
+        public static FoodConfig parse(String foodConfigString) {
+            String[] parts = foodConfigString.split("\\|", 3);
+            if (parts.length != 3) {
+                logger.warn("Could not parse food config from '{}'", foodConfigString);
+                return null;
+            }
+            try {
+                return new FoodConfig(parts[0], Integer.parseInt(parts[1]),
+                        Float.parseFloat(parts[2]));
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+
+    }
+
 }
--- src/com/hea3ven/hardmodetweaks/config/Config.java
+++ src/com/hea3ven/hardmodetweaks/config/Config.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.common.collect.Lists;
+
 import org.apache.logging.log4j.Level;
 
 import net.minecraft.world.GameRules;
@@ -17,6 +19,7 @@ import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 
+import com.hea3ven.hardmodetweaks.EatingRegenManager.FoodConfig;
 import com.hea3ven.tweaks.DayNightCycle;
 import com.hea3ven.tweaks.Hea3venTweaks;
 
@@ -27,6 +30,7 @@ public class Config {
 	public static int requiredFoodValue;
 	public static float healValueOffset;
 	public static float healValueMultiplier;
+	public static List<FoodConfig> foodValuesConfig;
 
 	public static boolean enableMobsTweaks;
 	public static boolean spidersApplySlowness;
@@ -57,6 +61,7 @@ public class Config {
 	private Property requiredFoodValueProp;
 	private Property healValueOffsetProp;
 	private Property healValueMultiplierProp;
+	private Property foodValuesConfigProp;
 
 	private Property enableSleepPreventionProp;
 	private Property sleepPreventionTimeoutProp;
@@ -134,6 +139,24 @@ public class Config {
 				.get("FoodHealing", "healValueMultiplier", 0.3d,
 						"The formula for the heal is (FoodValue - healValueOffset) * healValueMultiplier")
 				.setLanguageKey("hardmodetweaks.config.foodheal.healmultiplier");
+		foodValuesConfigProp = generalConfig
+				.get("FoodHealing", "foodValues",
+						new String[] {"minecraft:golden_carrot|8|14.4",
+								"minecraft:pumpkin_pie|8|12.0", "minecraft:golden_apple|10|9.6",
+								"minecraft:mushroom_stew|8|11", "minecraft:bread|6|11",
+								"minecraft:cooked_salmon|7|6.4", "minecraft:cooked_fish|5|7.8",
+								"minecraft:spider_eye|4|6.4", "minecraft:baked_potato|5|5.2",
+								"minecraft:apple|4|5.2", "minecraft:cooked_beef|3|5.2",
+								"minecraft:cooked_porkchop|3|5.2", "minecraft:cooked_chicken|3|5.2",
+								"minecraft:cookie|4|3.2", "minecraft:carrot|4|2.4",
+								"minecraft:melon|4|2.4", "minecraft:potato|4|2.4",
+								"minecraft:rotten_flesh|4|0.8", "minecraft:raw_salmon|2|2.4",
+								"minecraft:raw_fish|2|2.4", "minecraft:porkchop|2|2.4",
+								"minecraft:beef|2|2.4", "minecraft:chicken|2|2.4",
+								"minecraft:poisonous_potato|2|0.8"},
+						"Modifications to food's value and saturation."
+								+ " Remove all entries to leave as default vanilla values.")
+				.setLanguageKey("hardmodetweaks.config.foodheal.foodValues");
 
 		enableTweakAnimalAIProp = generalConfig
 				.get("Other", "enableAnimalAITweak", true,
@@ -217,6 +240,12 @@ public class Config {
 		Config.requiredFoodValue = requiredFoodValueProp.getInt();
 		Config.healValueOffset = (float) healValueOffsetProp.getDouble();
 		Config.healValueMultiplier = (float) healValueMultiplierProp.getDouble();
+		foodValuesConfig = Lists.newArrayList();
+		for (String foodConfigString : foodValuesConfigProp.getStringList()) {
+			FoodConfig foodConfig = FoodConfig.parse(foodConfigString);
+			if (foodConfig != null)
+				foodValuesConfig.add(foodConfig);
+		}
 
 		Config.enableMobsTweaks = enableMobsTweaksProp.getBoolean();
 		Config.spidersApplySlowness = spidersApplySlownessProp.getBoolean();
