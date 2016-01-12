64292c7520320a9b7e3716cf5bd73295166f03e5
Hea3veN <hea3venmc@gmail.com>
2016-01-12 18:41:26 -0300

    Add FMLCorePluginContainsFMLMod to the manifest

--- src/main/java/com/hea3ven/hardmodetweaks/ModHardModeTweaks.java
+++ src/main/java/com/hea3ven/hardmodetweaks/ModHardModeTweaks.java
@@ -26,10 +26,9 @@ import java.io.File;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -41,50 +40,50 @@ import com.hea3ven.hardmodetweaks.config.Config;
 import com.hea3ven.hardmodetweaks.config.ProjectConfig;
 
 @Mod(modid = "hardmodetweaks|main", version = ProjectConfig.version,
-        dependencies = "required-after:Forge@[" + ProjectConfig.forge_version + ",)",
-        guiFactory = "com.hea3ven.hardmodetweaks.config.HardModeTweaksGuiFactory")
+		dependencies = "required-after:Forge@[" + ProjectConfig.forge_version + ",)",
+		guiFactory = "com.hea3ven.hardmodetweaks.config.HardModeTweaksGuiFactory")
 public class ModHardModeTweaks {
 
-    private Logger logger = LogManager.getLogger("HardModeTweaks.Mod");
+	private Logger logger = LogManager.getLogger("HardModeTweaks.Mod");
 
-    @Instance("hardmodetweaks")
-    public static ModHardModeTweaks instance;
+	@Instance("hardmodetweaks")
+	public static ModHardModeTweaks instance;
 
-    @SidedProxy(clientSide = "com.hea3ven.hardmodetweaks.HardModeTweaksCommonProxy",
-            serverSide = "com.hea3ven.hardmodetweaks.HardModeTweaksCommonProxy")
-    public static HardModeTweaksCommonProxy proxy;
+	@SidedProxy(clientSide = "com.hea3ven.hardmodetweaks.HardModeTweaksCommonProxy",
+			serverSide = "com.hea3ven.hardmodetweaks.HardModeTweaksCommonProxy")
+	public static HardModeTweaksCommonProxy proxy;
 
-    public ModHardModeTweaks() {
-    }
+	public ModHardModeTweaks() {
+	}
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
 
-        logger.info("Loading config");
-        Config.init(new File(event.getModConfigurationDirectory(), "hardmodetweaks.cfg"));
-        FMLCommonHandler.instance().bus().register(this);
-    }
+		logger.info("Loading config");
+		Config.init(new File(event.getModConfigurationDirectory(), "hardmodetweaks.cfg"));
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
-    @EventHandler
-    public void modInit(FMLInitializationEvent event) {
-        onConfigChanged(null);
-    }
+	@Mod.EventHandler
+	public void modInit(FMLInitializationEvent event) {
+		onConfigChanged(null);
+	}
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-    }
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+	}
 
-    @SubscribeEvent
-    public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent eventArgs) {
-        if (eventArgs == null || eventArgs.modID.equals("hardmodetweaks|main")) {
-            logger.info("Reloading config");
-            Config.reload();
-            AITweaksManager.onConfigChanged();
-            EatingRegenManager.onConfigChanged();
-            MobsTweaksManager.onConfigChanged();
-            HardModeRulesManager.onConfigChanged();
-            SleepManager.onConfigChanged();
-            WorldTweaksManager.onConfigChanged();
-        }
-    }
+	@SubscribeEvent
+	public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent eventArgs) {
+		if (eventArgs == null || eventArgs.modID.equals("hardmodetweaks|main")) {
+			logger.info("Reloading config");
+			Config.reload();
+			AITweaksManager.onConfigChanged();
+			EatingRegenManager.onConfigChanged();
+			MobsTweaksManager.onConfigChanged();
+			HardModeRulesManager.onConfigChanged();
+			SleepManager.onConfigChanged();
+			WorldTweaksManager.onConfigChanged();
+		}
+	}
 }
