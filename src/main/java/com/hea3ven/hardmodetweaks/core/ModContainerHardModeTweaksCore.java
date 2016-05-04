dc4856b8f5123ca01f65bc5e62198319771ee32a
Hea3veN <hea3venmc@gmail.com>
2016-05-04 16:27:51 -0300

    Code cleanup

--- src/main/java/com/hea3ven/hardmodetweaks/core/ModContainerHardModeTweaksCore.java
+++ src/main/java/com/hea3ven/hardmodetweaks/core/ModContainerHardModeTweaksCore.java
@@ -21,8 +21,6 @@
 
 package com.hea3ven.hardmodetweaks.core;
 
-import java.util.Arrays;
-
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.eventbus.EventBus;
@@ -36,32 +34,31 @@ import net.minecraftforge.fml.common.ModMetadata;
 
 public class ModContainerHardModeTweaksCore extends DummyModContainer {
 
-    private Logger logger = LogManager.getLogger("HardModeTweaks.ModContainerHardModeTweaksCore");
+	private static Logger logger = LogManager.getLogger("HardModeTweaks.ModContainerHardModeTweaksCore");
 
-    public ModContainerHardModeTweaksCore() {
-        super(new ModMetadata());
+	public ModContainerHardModeTweaksCore() {
+		super(new ModMetadata());
 
-        logger.debug("configuring the mod");
+		logger.debug("configuring the mod");
 
-        ModMetadata meta = getMetadata();
-        meta.modId = "hardmodetweakscore";
-        meta.name = "Hard Mode Tweaks Core";
-        meta.version = "1.0.0";
-        meta.authorList = Arrays.asList("Hea3veN");
-        meta.description = "Core mod for Hard Mode Tweaks";
-        meta.url = "https://github.com/hea3ven/HardModeTweaks";
-        meta.screenshots = new String[0];
-        // meta.logoFile = "/hmt_logo.png";
-        meta.parent = "hardmodetweaks";
+		ModMetadata meta = getMetadata();
+		meta.modId = "hardmodetweakscore";
+		meta.name = "Hard Mode Tweaks Core";
+		meta.version = "1.0.0";
+		meta.authorList = Lists.newArrayList("Hea3veN");
+		meta.description = "Core mod for Hard Mode Tweaks";
+		meta.url = "https://github.com/hea3ven/HardModeTweaks";
+		meta.screenshots = new String[0];
+		// meta.logoFile = "/hmt_logo.png";
+		meta.parent = "hardmodetweaks";
 
-        meta.requiredMods = Sets.newHashSet();
-        meta.dependencies = Lists.newArrayList();
-        meta.dependants = Lists.newArrayList();
+		meta.requiredMods = Sets.newHashSet();
+		meta.dependencies = Lists.newArrayList();
+		meta.dependants = Lists.newArrayList();
+	}
 
-    }
-
-    @Override
-    public boolean registerBus(EventBus bus, LoadController controller) {
-        return true;
-    }
+	@Override
+	public boolean registerBus(EventBus bus, LoadController controller) {
+		return true;
+	}
 }
