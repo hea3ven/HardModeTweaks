5426ca718280631c2cfa36b73af39a74c09b3d3a
Hea3veN <hea3venmc@gmail.com>
2016-05-11 20:18:16 -0300

    * Fix the bed placement tracking not persisting through restarts

--- src/main/java/com/hea3ven/hardmodetweaks/sleep/SleepManager.java
+++ src/main/java/com/hea3ven/hardmodetweaks/sleep/SleepManager.java
@@ -1,12 +1,11 @@
 package com.hea3ven.hardmodetweaks.sleep;
 
-import java.util.HashSet;
-import java.util.Set;
+import javax.annotation.Nonnull;
 
 import net.minecraft.block.BlockBed;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 
 import net.minecraftforge.common.util.BlockSnapshot;
 import net.minecraftforge.event.entity.player.PlayerSleepInBedEvent;
@@ -17,53 +16,35 @@ public class SleepManager {
 
 	public static int sleepPreventionTimeout = 3;
 
-	public static class BlockPlacement {
-
-		public long worldTime;
-		public BlockPos pos;
-
-		public BlockPlacement(long worldTime, BlockPos pos) {
-			this.worldTime = worldTime;
-			this.pos = pos;
-		}
-	}
-
-	private Set<BlockPlacement> bedPlacements = new HashSet<>();
-
 	@SubscribeEvent
 	public void blockPlaceEvent(BlockEvent.MultiPlaceEvent event) {
 		if (event.getPlacedBlock().getBlock() instanceof BlockBed) {
+			BedPlacements bedPlacements = getBedPlacements(event.getWorld());
 			for (BlockSnapshot snap : event.getReplacedBlockSnapshots()) {
-				BlockPlacement placement = getPlacement(snap.getPos());
-				if (placement != null)
-					placement.worldTime = event.getWorld().getTotalWorldTime();
-				else
-					bedPlacements.add(
-							new BlockPlacement(event.getWorld().getTotalWorldTime(), snap.getPos()));
+				bedPlacements.onBedPlaced(event.getWorld(), snap.getPos());
 			}
 		}
 	}
 
 	@SubscribeEvent
 	public void sleep(PlayerSleepInBedEvent event) {
-		BlockPlacement bedPlacement = getPlacement(event.getPos());
-		if (bedPlacement == null)
+		BedPlacements bedPlacements = getBedPlacements(event.getEntity().worldObj);
+		if (bedPlacements.canSleep(event.getEntity(), event.getPos()))
 			return;
-		if (event.getEntity().worldObj.getTotalWorldTime() - bedPlacement.worldTime <
-				sleepPreventionTimeout) {
-			event.getEntityPlayer().addChatComponentMessage(
-					new TextComponentTranslation("tile.bed.recentlyPlaced"));
-			event.setResult(EntityPlayer.EnumStatus.OTHER_PROBLEM);
-		} else {
-			bedPlacements.remove(bedPlacement);
-		}
+
+		event.getEntityPlayer()
+				.addChatComponentMessage(new TextComponentTranslation("tile.bed.recentlyPlaced"));
+		event.setResult(EntityPlayer.EnumStatus.OTHER_PROBLEM);
 	}
 
-	private BlockPlacement getPlacement(BlockPos pos) {
-		for (BlockPlacement bedPlacement : bedPlacements) {
-			if (pos.equals(bedPlacement.pos))
-				return bedPlacement;
+	@Nonnull
+	private BedPlacements getBedPlacements(World world) {
+		BedPlacements bedPlacements =
+				(BedPlacements) world.loadItemData(BedPlacements.class, "beds_data");
+		if (bedPlacements == null) {
+			bedPlacements = new BedPlacements("beds_data");
+			world.setItemData("beds_data", bedPlacements);
 		}
-		return null;
+		return bedPlacements;
 	}
 }
